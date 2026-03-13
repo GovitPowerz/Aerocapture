83ff9c4178fd750d184fe747cc35a0d8c17680b1
Greg <gregory@powerz.tech>
2026-03-13 17:22:30 +0100

    Clean-ups

--- check_all.sh
+++ check_all.sh
@@ -3,5 +3,9 @@ cargo clean
 cargo test --quiet
 cargo fmt --all -- --check
 cargo clippy --all-targets --all-features --quiet -- -D warnings
-cargo clean
+# Copy binary out, clean everything, put it back
+cp target/release/aerocapture /tmp/ && \
+cargo clean && \
+mkdir -p target/release && \
+cp /tmp/aerocapture target/release/ && \
 cd ../..
